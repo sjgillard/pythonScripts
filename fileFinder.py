64af2ca9fa775dd8ac178b8122ebe1e722992128
Shona Gillard <sjgillard93@gmail.com>
2016-04-12 04:39:56 -0700

    Create fileFinder.py
    This is to have some functionality to find files or directories based on queries

---
+++ fileFinder.py
@@ -0,0 +1,28 @@
+# File finder
+import os
+
+class FileFinder(object):
+	def __init__(self, files, directory):
+		self.file = files
+		self.directory = directory
+
+	def findLog(self): # Find log file for given asset or project
+		pass
+
+	def findAssetFile(self): # Find asset file path
+		pass
+
+	def findProject(self): # Find asset project
+		pass
+
+	def findDirectory(self): # Find thedirectory or directories of any file given can be found in
+		pass
+
+	def findActiveProjects(self): # Finds all projects labeled active
+		pass
+
+	def findInactiveProjects(self): # Finds all projects labeled Inactive/Archived
+		pass
+
+	def getFileList(self): # Gets a list of files for any project or directory
+		pass
