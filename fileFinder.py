dd69f3a2016a7821e4c48f4a92609076092ab985
Shona Gillard <sjgillard93@gmail.com>
2016-04-15 01:18:37 -0700

    Updates
    Fleshing out the current functions to locate files.

--- fileFinder.py
+++ fileFinder.py
@@ -2,27 +2,58 @@
 import os
 
 class FileFinder(object):
-	def __init__(self, files, directory):
-		self.file = files
+	def __init__(self, directory):
 		self.directory = directory
 
 	def findLog(self): # Find log file for given asset or project
-		pass
+		for dirName, subdirList, fileList in os.walk(self.directory):
+			for file in fileList:
+				if 'Log.json' in file:
+					currDir = os.getcwd()
+					return os.path.join(currDir, file)
 
-	def findAssetFile(self): # Find asset file path
-		pass
+	def findAssetFile(self, asset): # Find asset file path
+		assetFiles = []
+
+		for dirName, subdirList, fileList in os.walk(self.directory):
+			for file in fileList:
+				if asset in file:
+					assetFiles.append(file)
+
+		return assetFiles
 
 	def findProject(self): # Find asset project
 		pass
 
-	def findDirectory(self): # Find thedirectory or directories of any file given can be found in
-		pass
+	def findDirectory(self, findFile): # Find the directory or directories of any file given can be found in
+		for dirName, subdirList, fileList in os.walk(self.directory):
+			for file in fileList:
+				if findFile in file:
+					return os.path.dirname(file)
 
 	def findActiveProjects(self): # Finds all projects labeled active
-		pass
+		active = os.path.join(self.directory, 'Active')
+
+		activeProjects = os.listdir(active)
+		return activeProjects
 
 	def findInactiveProjects(self): # Finds all projects labeled Inactive/Archived
-		pass
+		inactive = os.path.join(self.directory, 'Inactive')
 
-	def getFileList(self): # Gets a list of files for any project or directory
-		pass
+		inactiveProjects = os.listdir(inactive)
+		return inactiveProjects
+
+	def getFileList(self, dirType): # Gets a list of files for any project or directory
+		if 'project' in dirType:
+			log = findLog()
+			with open(log, 'r') as logFile:
+				data = json.load(logFile)
+
+			return data['Asset List']
+		else:
+			fileList = []
+			for dirName, subdirList, fileList in os.walk(self.directory):
+				for file in fileList:
+					fileList.append(file)
+
+			return fileList
